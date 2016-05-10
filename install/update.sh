f14be1c27dd0fa093f207c0a8165544f804659ff
chennanrun <naryn_chen@qq.com>
2016-05-11 03:11:36 +0800

    install nginx

---
+++ install/update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+rm -rf master
+rm -rf swoole_lottery-master
+
+yum install wget unzip -y
+
+wget https://codeload.github.com/naryn/swoole_lottery/zip/master
+unzip master
+unlink swoole_lottery
+ln -s  swoole_lottery-master swoole_lottery
+
+chmod +x /web/swoole_lottery/install/centos/*
+
+cd /web/swoole_lottery/install/centos/
+
+
+#docker run -itd -p 9876:80 192.168.62.187:5000/centos:6 /bin/bash
