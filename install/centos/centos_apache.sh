7f1cf42dfddc817a4424ea83b196fb41680d06a6
chennanrun <naryn_chen@qq.com>
2016-05-11 01:24:39 +0800

    install nginx

--- install/centos/centos_apache.sh
+++ install/centos/centos_apache.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #######install apache##########
-
+yum install gcc gcc-c++ -y
 #http://httpd.apache.org/download.cgi
 
 cd /usr/local/src/
@@ -9,30 +9,31 @@ cd /usr/local/src/
 if [ ! -f "/usr/local/src/apr-1.5.2.tar.gz" ]; then
   wget -O apr-1.5.2.tar.gz https://github.com/apache/apr/archive/1.5.2.tar.gz
 fi
+rm -rf apr-1.5.2
 tar zvxf apr-1.5.2.tar.gz
 cd apr-1.5.2
 ./configure --prefix=/usr/local/apr
 make
 make install
 
-
+cd /usr/local/src/
 if [ ! -f "/usr/local/src/apr-util-1.5.4.tar.gz" ]; then
   wget -O apr-util-1.5.4.tar.gz https://github.com/apache/apr-util/archive/1.5.4.tar.gz
 fi
+rm -rf apr-util-1.5.4
 tar zvxf apr-util-1.5.4.tar.gz
 cd apr-util-1.5.4
 ./configure --prefix=/usr/local/apr-util
 make
 make install
 
-
+cd /usr/local/src/
 if [ ! -f "/usr/local/src/httpd-2.4.20.tar.gz" ]; then
   wget http://apache.fayea.com//httpd/httpd-2.4.20.tar.gz
 fi
 rm -rf httpd-2.4.20
 tar -zvxf httpd-2.4.20.tar.gz
 cd httpd-2.4.20
-yum install gcc gcc-c++ -y
 
 
 ./configure \
@@ -49,5 +50,6 @@ yum install gcc gcc-c++ -y
 
 make
 make install
+unlink /usr/local/apache
 ln -s /usr/local/apache2.4.20 /usr/local/apache
 sudo /usr/local/apache/bin/httpd -k start
